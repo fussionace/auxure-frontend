2c0685b906f17ecb9a81f00b854a39ac92318a63
davidfayanju1 <davidfayanju01@gmail.com>
2023-06-29 09:00:03 +0100

    changes on initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Signup from './pages/authPages/Signup';
+import './index.css'
+
 
 import Home from './pages/unAuthPages/Home';
 import ErrorPage from './pages/unAuthPages/NotFound';
 import { home } from './utils/routes';
+import { signup } from './utils/routes';
 
 export const router = createBrowserRouter([
   {
     path: home,
     element: <Home />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: signup,
+    element: <Signup />,
   },
 ]);
--- src/pages/authPages/Signup.jsx
+++ src/pages/authPages/Signup.jsx
@@ -11,12 +11,12 @@ const Signup = () => {
         </aside>
         <main className='min-h-[100vh] form-content md:w-[50%] w-full bg-accent-2 flex flex-col items-center justify-center py-[1.8rem]'>
             <div className="form-title text-center mb-[3rem]">
-                <h1 className='font-primary text-text-lighter mb-[1rem] md:text-[2.6rem] text-[2.1rem]'>Create an account</h1>
-                <span className="font-primary block text-text-lighter text-[1rem]">Get started with an account on Auxure</span>
+                <h1 className='font-primary text-lighter mb-[1rem] md:text-[2.6rem] text-[2.1rem]'>Create an account</h1>
+                <span className="font-primary block text-lighter text-[1rem]">Get started with an account on Auxure</span>
             </div>
             {/* Google signup */}
             <div className="google-signup md:max-w-[25rem] w-[85%]">
-                <button className='h-[2.8rem] w-full flex border-solid border-[1px] font-semibold border-primary-color bg-accent-3 rounded-[5px] items-center justify-center gap-[1rem] text-[.9rem]'>
+                <button className='h-[2.8rem] w-full flex border-solid border-[1px] font-semibold border-primary bg-accent-3 rounded-[5px] items-center justify-center gap-[1rem] text-[.9rem]'>
                     <Google />
                     SIGN UP WITH GOOGLE
                 </button>
@@ -26,19 +26,19 @@ const Signup = () => {
             {/* main form elements */}
             <form className='md:max-w-[25rem] w-[85%]'>
                 <div className="form-element">
-                    <input type="email" className="h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-text-color outline-none text-[16px] border-primary-color" placeholder='Email address'/>
+                    <input type="email" className="h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-color outline-none text-[16px] border-primary" placeholder='Email address'/>
                 </div>
                 <div className="form-element">
-                    <input type="password" className="my-[2rem] h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-text-color outline-none text-[16px] outline-hidden border-primary-color" placeholder='Password'/>
+                    <input type="password" className="my-[2rem] h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-color outline-none text-[16px] outline-hidden border-primary" placeholder='Password'/>
                 </div>
                 <div className="form-element mb-[3rem]">
-                    <input type="password" className="mb-[.5rem] h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-text-color outline-none text-[16px] border-primary-color" placeholder='Confirm Password'/>
+                    <input type="password" className="mb-[.5rem] h-[2.8rem] w-full border-solid border-[1px] rounded-[5px] px-[2rem] placeholder:text-color outline-none text-[16px] border-primary" placeholder='Confirm Password'/>
                     <small className='text-left block font-primary'>8 or more characters</small>
                 </div>
                 <div className="form-submit-button">
-                    <p className='font-primary'>By signing up you agree with out <span className='text-primary-color font-semibold'><Link to='/'>Terms & Conditions</Link></span></p>
-                    <button className='outline-hidden md:my-[1.2rem] my-[.9rem] h-[2.8rem] w-full rounded-[5px] border-none bg-primary-color text-white-color font-semibold text-[.9rem]'>SIGN UP</button>
-                    <p className='text-center'>Already have an account <span className='text-primary-color font-semibold'><Link to='/login'>Log In</Link></span></p>   
+                    <p className='font-primary text-[.9rem]'>By signing up you agree with out <span className='text-primary font-semibold'><Link to='/'>Terms & Conditions</Link></span></p>
+                    <button className='outline-hidden md:my-[1.2rem] my-[.9rem] h-[2.8rem] w-full rounded-[5px] border-none bg-primary text-white font-semibold text-[.9rem]'>SIGN UP</button>
+                    <p className='text-center text-[.9rem]'>Already have an account <span className='text-primary font-semibold'><Link to='/login'>Log In</Link></span></p>   
                 </div>
             </form>
         </main>
