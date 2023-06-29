88a2d1a1dbcce42f7096b3c1c3a1923bc646b7f6
davidfayanju1 <davidfayanju01@gmail.com>
2023-06-29 08:08:20 +0100

    Changes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { ReactComponent as AuxureLogo } from './assets/svg/logo.svg';
+import { createBrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-background-color'>
-      <AuxureLogo className='' />
-      {/* <h1 className='font-secondary text-primary-color'> */}
-      <h1 className='text-primary-color leading-2 text-xl font-semibold uppercase font-secondary '>
-        Discover The Essence Of Timeless Luxury
-      </h1>
-    </div>
-  );
-}
+import Home from './pages/unAuthPages/Home';
+import ErrorPage from './pages/unAuthPages/NotFound';
+import { home } from './utils/routes';
 
 export const router = createBrowserRouter([
   {
