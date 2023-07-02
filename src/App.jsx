b4a0dce1b1d2df604445ead91ac38d485f71b7ac
Chivosmart <abdullahiyusufademola@gmail.com>
2023-07-03 00:28:00 +0100

    secon commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-    // <div className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-background-color'>
-    //   <AuxureLogo className='' />
-    //   <h1 className='text-primary-color leading-2 text-xl font-semibold uppercase font-secondary '>
-    //     Discover The Essence Of Timeless Luxury
-    //   </h1>
-    //   <Login />
-    // </div>
-    <div>
+    <div className='w-full h-screen flex flex-col justify-center items-center gap-4 bg-background-color'>
+      <AuxureLogo className='' />
+      <h1 className='text-primary-color leading-2 text-xl font-semibold uppercase font-secondary '>
+        Discover The Essence Of Timeless Luxury
+      </h1>
       <Login />
     </div>
   );
